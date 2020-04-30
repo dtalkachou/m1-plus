3e9f38bf98fbf1ca5c5bbf7c8eab4e6786e43cea
Dzianis Talkachou <dtalkachou@ya.ru>
2020-04-30 23:58:34 +0300

    Observer disconnect added

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,25 @@
 const user_ids = [];
 
-const node = document.getElementsByTagName('body')[0];
-
 const observer = new MutationObserver(records => {
-	var re = /^player_card_(\d+)$/;
+	let re = /^player_card_(\d+)$/;
 	records.forEach(function(arg) {
-		arg.addedNodes.forEach(function(arg) {
-			var user_id = arg.id && re.test(arg.id) && arg.id.match(re)[1];
+		if (arg.target.classList.contains('table-body-players')) {
+			let addedNode = arg.addedNodes[0];
+			var user_id = addedNode.id && re.test(addedNode.id) && addedNode.id.match(re)[1];
 			if (user_id) {
 				user_ids.push(user_id);
 			}
-		});
+		}
+		else if (arg.target.id && arg.target.id === 'styles-dyn-css' && user_ids.length) {
+			// Scripts on page have two mutation with #styles-dyn-css.
+			// First mutation before muttation with .table-body-players, second after
+			observer.disconnect();
+			console.log(user_ids);
+		}
 	});
 });
 
-observer.observe(node, {
+observer.observe(document.getElementsByTagName('body')[0], {
 	childList : true,
 	subtree: true
 });
