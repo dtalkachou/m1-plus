46442d4c8db09401bf34c2d619dd46d0f605eeeb
Dzianis Talkachou <dtalkachou@ya.ru>
2020-05-01 18:58:01 +0300

    .table-body-players-card-body-avatar>div padding added

--- contentScript.js
+++ contentScript.js
@@ -18,13 +18,30 @@ const observer = new MutationObserver(records => {
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
-					let str = '';
 					var responseData = JSON.parse(this.responseText).data;
+
+					var fileref = document.createElement('link')
+					fileref.setAttribute('rel', 'stylesheet')
+					fileref.setAttribute('type', 'text/css')
+					fileref.setAttribute('href', 'https://dtalkachou.github.io/m1-plus/css/rewrited-table-new.css')
+					document.getElementsByTagName("head")[0].appendChild(fileref)
+					
 					responseData.forEach(function(el) {
-						let winRate = el.games_wins / el.games * 100 || 0;
-						str += el.nick.concat(" - ", el.hasOwnProperty('rank') ? el.rank.title : 'Без звания', ' - ( ', el.games_wins, ' / ', el.games, ' = ', Math.round(winRate), '% )\n');
+						// let winRate = el.games_wins / el.games * 100 || 0;
+
+						let cardBodyInfoDiv = document.createElement('div')
+						cardBodyInfoDiv.className = 'table-body-players-card-body-info'
+						let cardBodyDiv = document.querySelector('.table-body-players-card-body')
+						cardBodyDiv.insertBefore(cardBodyInfoDiv, cardBodyDiv.firstChild)
+						document.getElementsByTagName("head")[0].appendChild(fileref)
+
+						cardBodyInfoDiv.appendChild(document.querySelector('.table-body-players-card-body-avatar'))
+						// let playerCardStatsDiv = document.createElement('div');
+						// playerCardStatsDiv.className = ''
+
+						// str += el.nick.concat(" - ", el.hasOwnProperty('rank') ? el.rank.title : 'Без звания', ' - ( ', el.games_wins, ' / ', el.games, ' = ', Math.round(winRate), '% )\n');
 					})
-					alert(str);
+					// alert(str);
 				}
 			};
 			xhttp.open('GET', 'https://monopoly-one.com/api/users.get?user_ids=' + user_ids.join(','), true);
@@ -32,6 +49,7 @@ const observer = new MutationObserver(records => {
 		}
 	});
 });
+
 
 observer.observe(document.getElementsByTagName('body')[0], {
 	childList : true,
