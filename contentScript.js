31806b56dce3b87d5f51be7f6dc38697f948e1bc
Dzianis Talkachou <dtalkachou@ya.ru>
2020-05-07 03:33:45 +0300

    Pull request #2 remarks fixed

--- contentScript.js
+++ contentScript.js
@@ -3,11 +3,11 @@ const user_ids = [];
 console.log('worked')
 
 const observer = new MutationObserver(records => {
-	let re = /^player_card_(\d+)$/;
-	records.forEach(function(arg) {
+	const re = /^player_card_(\d+)$/;
+	records.forEach((arg) => {
 		if (arg.target.classList.contains('table-body-players')) {
 			let addedNode = arg.addedNodes[0]
-			var user_id = addedNode.id && re.test(addedNode.id) && addedNode.id.match(re)[1]
+			let user_id = addedNode.id && re.test(addedNode.id) && addedNode.id.match(re)[1]
 			if (user_id) {
 				user_ids.push(user_id)
 			}
@@ -17,15 +17,17 @@ const observer = new MutationObserver(records => {
 			// First mutation before muttation with .table-body-players, second after
 			observer.disconnect();
 
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
+			fetch('https://monopoly-one.com/api/users.get?user_ids=' + user_ids.join(','))
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
 					let fileref = document.createElement('link')
 					fileref.setAttribute('rel', 'stylesheet')
 					fileref.setAttribute('type', 'text/css')
 					fileref.setAttribute('href', 'https://dtalkachou.github.io/m1-plus/css/rewrited-table-new.css')
 					document.getElementsByTagName("head")[0].appendChild(fileref)
-
+					
 					let playersCardBodys = document.getElementsByClassName('table-body-players-card-body')
 					for (let item of playersCardBodys) {
 						let cardBodyInfoDiv = document.createElement('div')
@@ -46,19 +48,15 @@ const observer = new MutationObserver(records => {
 						cardBodyStatsDiv.appendChild(cardBodyStatsGamesDiv)
 					}
 
-					var responseData = JSON.parse(this.responseText).data;
-					responseData.forEach(function(el) {
-						let playerCard = document.getElementById('player_card_' + el.user_id)
-						if (el.hasOwnProperty('rank')) {
-							playerCard.querySelector('.table-body-players-card-body-stats-rank').style.backgroundImage = `url("${el.rank.img}")`
+					data.data.forEach((user) => {
+						let playerCard = document.getElementById('player_card_' + user.user_id)
+						if (user.hasOwnProperty('rank')) {
+							playerCard.querySelector('.table-body-players-card-body-stats-rank').style.backgroundImage = `url("${user.rank.img}")`
 						}
-						let winRate = el.games_wins / el.games * 100 || 0;
-						playerCard.querySelector('.table-body-players-card-body-stats-games').innerHTML = `${el.games_wins} / ${el.games}<span>(${Math.round(winRate)}%)</span>`
+						let winRate = user.games_wins / user.games * 100 || 0;
+						playerCard.querySelector('.table-body-players-card-body-stats-games').innerHTML = `${user.games_wins} / ${user.games}<span>(${Math.round(winRate)}%)</span>`
 					})
-				}
-			}
-			xhttp.open('GET', 'https://monopoly-one.com/api/users.get?user_ids=' + user_ids.join(','), true)
-			xhttp.send()
+				})
 		}
 	})
 })
